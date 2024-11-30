efb7247aabd6036586f3a63876550053a3ad6515
David-Sang96 <luainawl@gmail.com>
2024-11-30 22:12:17 +0800

    courses done

---
+++ src/utils/course.js
@@ -0,0 +1,39 @@
+export const courseTitleByUrl = {
+  // For "master-basics"
+  "master-the-basics-korean-for-beginner":
+    "Master the Basics: Korean for Beginner",
+  "master-the-basics-thailand-for-beginner":
+    "Master the Basics: Thailand for Beginner",
+  "master-the-basics-japanese-for-beginner":
+    "Master the Basics: Japanese for Beginner",
+  "master-the-basics-spanish-for-beginner":
+    "Master the Basics: Spanish for Beginner",
+  "master-the-basics-english-for-beginner":
+    "Master the Basics: English for Beginner",
+
+  // For "elementary"
+  "elementary-korean-master-the-basics": "Elementary Korean: Master the Basics",
+  "elementary-thailand-master-the-basics":
+    "Elementary Thailand: Master the Basics",
+  "elementary-japanese-master-the-basics":
+    "Elementary Japanese: Master the Basics",
+  "elementary-spanish-master-the-basics":
+    "Elementary Spanish: Master the Basics",
+  "elementary-english-master-the-basics":
+    "Elementary English: Master the Basics",
+
+  // For "intermediate"
+  "intermediate-korean-core-mastery": "Intermediate Korean: Core Mastery",
+  "intermediate-thailand-core-mastery": "Intermediate Thailand: Core Mastery",
+  "intermediate-japanese-core-mastery": "Intermediate Japanese: Core Mastery",
+  "intermediate-spanish-core-mastery": "Intermediate Spanish: Core Mastery",
+  "intermediate-english-core-mastery": "Intermediate English: Core Mastery",
+};
+
+export const chartData = [
+  { number: 100 },
+  { number: 80 },
+  { number: 60 },
+  { number: 40 },
+  { number: 20 },
+];
