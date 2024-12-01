88a801db7d8d7540e130e0ad1dfdb9b95c26f6ca
khantMinHtut <khantminhtut009@gmail.com>
2024-12-01 12:32:47 +0630

    feat:fix big bugs

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -34,50 +34,12 @@ const Header = () => {
         <Link to="/" className="size-24 flex items-center justify-center">
           <Logo theme={"dark"} />
         </Link>
-      </div>
-      <div className="gap-4 items-center justify-center hidden md:flex"></div>
-
-      <nav className="flex justify-between items-center text-neutral-900 px-2  h-[80px]">
-        <Link to="/" className=" size-24 flex items-center justify-center ">
-          <Logo theme={"dark"} />
-        </Link>
-
-        <div className=" gap-4 items-center mx-auto justify-center hidden md:flex">
+        <div className="gap-4 items-center justify-center hidden md:flex">
           <NavLink to="/">Home</NavLink>
           <NavLink to="/courses">Courses</NavLink>
           <NavLink to="/about">About</NavLink>
           <NavLink to="/contact">Contact</NavLink>
         </div>
-
-        <div className=" flex gap-4 items-center justify-center">
-          <NavLink to="/become-an-instructor" className=" hidden md:flex">
-            Become an Instructor
-          </NavLink>
-          <div className=" flex gap-2 items-center justify-center relative">
-            <button>
-              <img src={userIccon} alt="userIcon" />
-            </button>
-            <button className=" md:hidden" onClick={() => setOpen(!open)}>
-              <img src={menuIcon} alt="menuIcon" />
-            </button>
-            {open && <Driopdwon setOpen={setOpen} />}
-          </div>
-        </div>
-      </nav>
-
-      <nav className=" w-full">
-        <div className="flex justify-between items-center text-neutral-900 px-2  h-[80px] max-w-[1208px] mx-auto">
-          <Link to="/" className=" size-24 flex items-center justify-center ">
-            <Logo theme={"dark"} />
-          </Link>
-        </div>
-        <div className=" gap-4 items-center justify-center hidden md:flex">
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/courses">Courses</NavLink>
-          <NavLink to="/about">About</NavLink>
-          <NavLink to="/contact">Contact</NavLink>
-        </div>
-
         <div className="flex gap-4 items-center justify-center">
           <NavLink to="/become-an-instructor" className="hidden md:flex">
             Become an Instructor
@@ -99,23 +61,8 @@ const Header = () => {
               </div>
             )}
           </div>
-
-          <div className=" flex gap-4 items-center justify-center ">
-            <NavLink to="/become-an-instructor" className=" hidden md:flex">
-              Become an Instructor
-            </NavLink>
-            <div className=" flex gap-2 items-center justify-center relative">
-              <button>
-                <img src={userIccon} alt="userIcon" />
-              </button>
-              <button className=" md:hidden" onClick={() => setOpen(!open)}>
-                <img src={menuIcon} alt="menuIcon" />
-              </button>
-              {open && <Driopdwon setOpen={setOpen} />}
-            </div>
-          </div>
         </div>
-      </nav>
+      </div>
     </nav>
   );
 };
