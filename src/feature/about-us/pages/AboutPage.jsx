d5f7b699b68aec7357f057473454701a90f0e20a
Yehtetaung158 <137622241+Yehtetaung158@users.noreply.github.com>
2024-12-21 00:57:57 +0630

    debut 3

--- src/feature/about-us/pages/AboutPage.jsx
+++ src/feature/about-us/pages/AboutPage.jsx
@@ -3,8 +3,8 @@ import FirstSection from "./components/FirstSection";
 import SecondSecting from "./components/SecondSection";
 import ThirdSection from "./components/ThirdSection";
 import FourthSection from "./components/FourthSection";
-import Container from "../../../components/Container";
-import FifthSection from "./components/FifthSection";
+// import Container from "../../../components/Container";
+// import FifthSection from "./components/FifthSection";
 
 const AboutPage = () => {
   return (
