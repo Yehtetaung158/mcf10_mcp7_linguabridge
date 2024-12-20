0a1706cf0552f97464bbbeffdf451d0c5d41ffaf
Yehtetaung158 <137622241+Yehtetaung158@users.noreply.github.com>
2024-12-21 00:33:22 +0630

    debut vercel one

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,15 @@
-// import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      '@': path.resolve(__dirname, 'src'), // Shortens '@/' to 'src/'
     },
   },
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Ensure output folder is specified
+  },
 });
-
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// });
