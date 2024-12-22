9377c39d43bf1415ef8a25474e5dd3e696eb6b34
Yehtetaung158 <137622241+Yehtetaung158@users.noreply.github.com>
2024-12-22 23:40:29 +0630

    debug_Aboutpage_2

--- src/feature/about-us/pages/AboutPage.jsx
+++ src/feature/about-us/pages/AboutPage.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import FirstSection from "../components/FirstSection";
 import SecondSection from "../components/SecondSection";
 import ThirdSection from "../components/ThirdSection";
 import FourthSection from "../components/FourthSection";
 import Container from "../../../components/Container";
 import FifthSection from "../components/FifthSection";
+import FirstSection from "../components/FristSection";
 
 const AboutPage = () => {
   return (
     <>
-      <FirstSection />
+      <FirstSection/>
       <SecondSection />
       <ThirdSection />
       <FourthSection />
