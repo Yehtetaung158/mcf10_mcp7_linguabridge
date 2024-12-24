275ef94a399277e0fc82e2fc9838644ec7d30c00
zett21 <rtt2001xx@gmail.com>
2024-12-24 22:31:46 +0630

    responsive fixed

--- src/feature/become-an-instructor/components/HeroSection.jsx
+++ src/feature/become-an-instructor/components/HeroSection.jsx
@@ -4,13 +4,13 @@ import img1 from "../../../assets/Become an instructor/instructor_hero.png";
 const HeroSection = () => {
   return (
     <section className="w-full  md:py-[120px] relative">
-      <main className="max-w-[1207px]  lg:flex justify-between  mx-auto px-5  max-lg:p-10">
-        <div className=" w-fit h-fit space-y-10  ">
+      <main className="max-w-[1207px]  lg:flex justify-between items-center  mx-auto px-5  max-lg:p-10">
+        <div className=" w-fit h-fit space-y-16 flex flex-col justify-end mt-auto ">
           <p className=" w-fit rounded-[4px] px-[8px] py-[4px] text-ps_font_size leading-[16.94px] text-neutral-900 bg-secondary-color">
             🚀 Transform Lives Through the Power of Language Education
           </p>
 
-          <h1 className=" w-fit font-header_font_style font-header_weight text-[35px] md:text-[60px] leading-[68px] lg:leading-[90px] text-neutral-900 ">
+          <h1 className=" w-fit items-center font-header_font_style font-header_weight text-[35px] md:text-[40px]  text-neutral-900 ">
             Join Our Community of Language Experts
           </h1>
           <p className="w-fit  text-[18px] leading-[27px] text-neutral-600">
@@ -22,7 +22,7 @@ const HeroSection = () => {
           </button>
         </div>
         <div className="  max-lg:mt-10">
-          <img src={img1} className="w-full h-full" alt="" />
+          <img src={img1} className="w-full " alt="" />
         </div>
       </main>
     </section>
